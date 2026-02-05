b9478be1ddb934741bcddd7c559b91c023759750
shahriyar moqimi <moqimishahriyar@gmail.com>
2026-02-05 18:31:48 +0330

    get input

--- src/main.py
+++ src/main.py
@@ -1,1 +1,62 @@
-#main.py
+import time
+from data_loader import build_graph_task1
+from utils import find_node_index
+from dijkstra_algo import run_dijkstra
+from lc_algo import run_label_correcting
+
+
+def main():
+    g = build_graph_task1('airports.dat', 'routes.dat')
+
+    print("\n--- Flight Route Optimization System ---")
+
+    while True:
+        print("=" * 50)
+        start_node = find_node_index(g, "🛫 Source city name: ")
+        target_node = find_node_index(g, "🛬 Destination city name: ")
+
+        if start_node == target_node:
+            print("❌ Source and destination cannot be the same!");
+            continue
+
+        print("\n" + "-" * 20 + " Results " + "-" * 20)
+
+        print("\n🔹 Method 1: Dijkstra (Array Implementation)")
+        t0 = time.time()
+
+        dist1, path1 = run_dijkstra(g, start_node, target_node)
+
+        t1 = time.time()
+
+        if dist1 >= 10 ** 9:
+            print("❌ No path found.")
+        else:
+            print(f"✅ Total distance: {dist1:.2f} km")
+            print(f"⏱️ Execution time: {t1 - t0:.6f} seconds")
+            print("📋 Suggested Path:")
+            for s, n in enumerate(path1):
+                info = g.node_info[n]
+                print(f"   {s + 1}. {info['name']} ({info['city']})")
+
+        print("\n🔸 Method 2: Label Correcting (Queue Implementation)")
+        t0 = time.time()
+
+        dist2, path2 = run_label_correcting(g, start_node, target_node)
+
+        t1 = time.time()
+
+        if dist2 == float('inf'):
+            print("❌ No path found.")
+        else:
+            print(f"✅ Total distance: {dist2:.2f} km")
+            print(f"⏱️ Execution time: {t1 - t0:.6f} seconds")
+            # Print if the path is different (usually they are the same)
+            if len(path1) != len(path2) and len(path1) > 0:
+                print("   (Found path is different)")
+
+        if input("\nDo you want to continue? (y/n): ").lower() != 'y':
+            break
+
+
+if __name__ == "__main__":
+    main()
