9492ee48e3d140ee42bca474e8da77db6f201d28
shahriyar moqimi <moqimishahriyar@gmail.com>
2026-02-05 18:29:27 +0330

    reading routes

--- src/data_loader.py
+++ src/data_loader.py
@@ -43,4 +43,28 @@ def build_graph_task1(airports_file, routes_file):
     for idx, info in temp_details.items():
         g.set_node_details(idx, info['name'], info['city'], info['country'], info['real_id'])
 
+    edge_count = 0
+    try:
+        with open(routes_file, 'r', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                try:
+                    src, dst = int(row[3]), int(row[5])
 
+                    if src in id_map and dst in id_map:
+                        u = id_map[src]
+                        v = id_map[dst]
+
+                        dist = haversine(coords[src][1], coords[src][0],
+                                         coords[dst][1], coords[dst][0])
+
+                        g.add_edge(u, v, dist)
+                        edge_count += 1
+                except:
+                    continue
+    except FileNotFoundError:
+        print(f"❌ File {routes_file} not found!");
+        sys.exit()
+
+    print(f"✅ Graph built with {current_idx} airports and {edge_count} routes.")
+    return g
