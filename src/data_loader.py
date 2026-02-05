983e56aa21b31a61cb9ed81930156125cf1e0639
shahriyar moqimi <moqimishahriyar@gmail.com>
2026-02-05 18:26:02 +0330

    graph builder

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,3 +2,13 @@ import csv
 import sys
 from graph import Graph
 from utils import haversine
+
+
+def build_graph_task1(airports_file, routes_file):
+
+    print("⏳ Reading files and building graph...")
+
+    id_map = {}
+    coords = {}
+    temp_details = {}
+    current_idx = 0
